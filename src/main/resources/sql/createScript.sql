08862185dec28aa74e6bf68f41ae9e41e9f0bd3a
vanamir2 <miravana10@seznam.cz>
2019-04-07 19:21:46 +0200

    Uprava createScriptu.

--- src/main/resources/sql/createScript.sql
+++ src/main/resources/sql/createScript.sql
@@ -1,17 +1,19 @@
 -- CREATE SCHEMA `ppj` ;
 
-drop table IF EXISTS City;
-drop table IF EXISTS Country;
+DROP TABLE IF EXISTS City;
+DROP TABLE IF EXISTS Country;
 
-Create table Country(
+CREATE TABLE Country
+(
     name VARCHAR(80) PRIMARY KEY NOT NULL
 );
 
-Create table City(
-                     name    VARCHAR(80) PRIMARY KEY NOT NULL,
-                     country VARCHAR(80)             NOT NULL,
-                     FOREIGN KEY (country)
-                         REFERENCES Country (name)
-                         ON DELETE CASCADE
+CREATE TABLE City
+(
+    name    VARCHAR(80) PRIMARY KEY NOT NULL,
+    country VARCHAR(80)             NOT NULL,
+    FOREIGN KEY (country)
+        REFERENCES Country (name)
+        ON DELETE CASCADE
 );
-commit;
+COMMIT;
